6ba50564520f92ffaf1e40e8bb24180801e54264
matsixfive <maffers2006@gmail.com>
2023-01-24 14:38:57 +0000

    init (format)

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,5 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  app.route("/api/stock-prices").get(function (req, res) {});
 };
